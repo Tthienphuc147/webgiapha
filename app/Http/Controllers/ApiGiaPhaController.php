523b539e6695bfa2cd45afa3e40fcd5cecc969ae
nghiadh2016 <47706371+nghiadh2016@users.noreply.github.com>
2019-11-08 12:36:15 +0700

    update logic giapha

--- app/Http/Controllers/ApiGiaPhaController.php
+++ app/Http/Controllers/ApiGiaPhaController.php
@@ -15,7 +15,7 @@ class ApiGiaPhaController extends Controller
         $husband=DB::table('husband')->where('id',$id)->get();
         $husband1=DB::table('husband')->get();
         $arrwife=DB::table('wife')->where('id_user',$id)->get();
-        $children1=DB::table('children')->where('id_user_dady',$id)->where('id_user_information','!=',-1)->get();
+        $children1=DB::table('children')->where('id_user_dady',$id)->where('id_user_information','!=',-1)->where('sex',1)->get();
         $listchildren1=DB::table('children')->where('id_user_dady',$id)->get();
         $arrwife1=array();
         if(sizeof($children1)>0)$arrwife1=DB::table('wife')->where('id_user',$children1[0]->id_user_dady)->get();
@@ -38,19 +38,19 @@ class ApiGiaPhaController extends Controller
         for($i=0;$i<sizeof($children1);$i++)
         {
             
-            $arr2[$i]=DB::table('children')->where('id_user_dady',$children1[$i]->id_user_information)->where('id_user_information','!=',-1)->get();
+            $arr2[$i]=DB::table('children')->where('id_user_dady',$children1[$i]->id_user_information)->where('id_user_information','!=',-1)->where('sex',1)->get();
             $arrwife2[$i]=DB::table('wife')->where('id_user',$children1[$i]->id_user_information)->get();
             $listchildren2[$i]=DB::table('children')->where('id_user_dady',$children1[$i]->id_user_information)->get();
             for($j=0;$j<sizeof($arr2[$i]);$j++){
-                $arr3[$i][$j]=DB::table('children')->where('id_user_dady',$arr2[$i][$j]->id_user_information)->where('id_user_information','!=',-1)->get();
+                $arr3[$i][$j]=DB::table('children')->where('id_user_dady',$arr2[$i][$j]->id_user_information)->where('id_user_information','!=',-1)->where('sex',1)->get();
                 $arrwife3[$i][$j]=DB::table('wife')->where('id_user',$arr2[$i][$j]->id_user_information)->get();
                 $listchildren3[$i][$j]=DB::table('children')->where('id_user_dady',$arr2[$i][$j]->id_user_information)->get();
                 for($k=0;$k<sizeof($arr3[$i][$j]);$k++){
-                    $arr4[$i][$j][$k]=DB::table('children')->where('id_user_dady',$arr3[$i][$j][$k]->id_user_information)->where('id_user_information','!=',-1)->get();
+                    $arr4[$i][$j][$k]=DB::table('children')->where('id_user_dady',$arr3[$i][$j][$k]->id_user_information)->where('id_user_information','!=',-1)->where('sex',1)->get();
                     $arrwife4[$i][$j][$k]=DB::table('wife')->where('id_user',$arr3[$i][$j][$k]->id_user_information)->get();
                     $listchildren4[$i][$j][$k]=DB::table('children')->where('id_user_dady',  $arr3[$i][$j][$k]->id_user_information)->get();
                     for($l=0;$l<sizeof($arr4[$i][$j][$k]);$l++){
-                        $arr5[$i][$j][$k][$l]=DB::table('children')->where('id_user_dady',$arr4[$i][$j][$k][$l]->id_user_information)->where('id_user_information','!=',-1)->get();
+                        $arr5[$i][$j][$k][$l]=DB::table('children')->where('id_user_dady',$arr4[$i][$j][$k][$l]->id_user_information)->where('id_user_information','!=',-1)->where('sex',1)->get();
                         $arrwife5[$i][$j][$k][$l]=DB::table('wife')->where('id_user',$arr4[$i][$j][$k][$l]->id_user_information)->get();
                         $listchildren5[$i][$j][$k][$l]=DB::table('children')->where('id_user_dady', $arr4[$i][$j][$k][$l]->id_user_information)->get();
                        
